4d5f38cb120dbd457dde274966122385299ea870
WMPL02 <prabodalakshan02@gmail.com>
2026-04-07 14:35:39 +0530

    Add mongo db url

--- flexit-backend/flexit/src/main/java/com/flexit/repository/ResourceRepository.java
+++ flexit-backend/flexit/src/main/java/com/flexit/repository/ResourceRepository.java
@@ -1,19 +1,9 @@
 package com.flexit.repository;
 
 import com.flexit.model.Resource;
-import com.flexit.model.ResourceType;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ResourceRepository extends MongoRepository<Resource, String> {
-
-    List<Resource> findByType(ResourceType type);
-
-    List<Resource> findByLocationContainingIgnoreCase(String location);
-
-    List<Resource> findByCapacityGreaterThanEqual(Integer capacity);
 }
-
